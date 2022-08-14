add0d036cbffd7d2594a13f683a7a422332f3201
xiutai <101174238+xiutai@users.noreply.github.com>
2022-08-14 19:15:43 +0800

    Update dengta.sh

--- dengta.sh
+++ dengta.sh
@@ -4,3 +4,7 @@ cd ARL/docker/
 docker volume create arl_db
 docker-compose pull
 docker-compose up -d 
+docker exec -ti arl_mongodb mongo -u admin -p admin
+use arl
+db.user.drop()
+db.user.insert({ username: 'admin',  password: hex_md5('arlsalt!@#'+'xiutai') })
