a057de3e8b8d21a10a2fd6bbec088959124be1a1
xiutai <101174238+xiutai@users.noreply.github.com>
2022-10-08 01:36:11 +0800

    Update awvs.sh

--- awvs.sh
+++ awvs.sh
@@ -11,7 +11,6 @@ cd
 echo "127.0.0.1    updates.acunetix.com" >> /etc/hosts
 echo "127.0.0.1    erp.acunetix.com" >> /etc/hosts
 echo "127.0.0.1    bxss.me" >> /etc/hosts
-echo "127.0.0.1    telemetry.invicti.com" >> /etc/hosts
 sudo apt-get update
 apt-get install libxdamage1 libgtk-3-0 libasound2 libnss3 libxss1 libgbm-dev -y
 # 下载文件：
@@ -19,12 +18,10 @@ curl -O https://scanadmin.com/pojie.rar
 curl -O https://scanadmin.com/awvs.rar
 unrar x -Pwww.ddosi.org awvs.rar
 unrar x -Pwww.ddosi.org pojie.rar
-# mkdir pojie
-# mv license_info.json wa_data.dat pojie
+mkdir pojie
+mv license_info.json wa_data.dat pojie
 apt install chmod
-chmod 777 acunetix_14.9.220713150_x64.sh
-./acunetix_14.9.220713150_x64.sh
-cd 14.8.x_acu_lic
-cp -p * /home/acunetix/.acunetix/data/license/
+chmod 777 acunetix_14.7.220425114_x64.sh
+./acunetix_14.7.220425114_x64.sh
 
 
