b62480757a6e238069093d6960f84e7119a0a810
xiutai <101174238+xiutai@users.noreply.github.com>
2022-12-05 15:10:59 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,7 +1,6 @@
 apt update
 apt install python3-pip zip -y
 pip install requests
-mkdir /root/spa
 cd /root/spa
 wget http://20.187.190.201:8080/wp-content/uploads/2022/12/spa-12.5.zip
 unzip spa-12.5.zip
